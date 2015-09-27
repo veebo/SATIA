89576fe18a15aac2c5a9cfde0fee444287dc8106
Viktor <victorbodrov@yandex.ru>
2015-09-27 14:21:49 +0300

    Initial structure

---
+++ src/groovy/com/mephiboys/satia/groovy/PageResolver.groovy
@@ -0,0 +1,17 @@
+package com.mephiboys.satia.groovy
+
+
+class PageResolver {
+
+    def a(){
+        def resp = 'aaa'
+        return resp;
+    }
+
+    def b(){
+        def resp = 'bbb'
+        return resp;
+    }
+
+}
+
