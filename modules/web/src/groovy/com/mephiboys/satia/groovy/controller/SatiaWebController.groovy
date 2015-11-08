d14d686d7bdec26d69d5dcbd4f6b9a6dee4f49eb
unknown <profgansmok@gmail.com>
2015-11-08 22:22:21 +0300

    fix errors

--- modules/web/src/groovy/com/mephiboys/satia/groovy/controller/SatiaWebController.groovy
+++ modules/web/src/groovy/com/mephiboys/satia/groovy/controller/SatiaWebController.groovy
@@ -12,13 +12,16 @@ import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.servlet.ModelAndView
+import org.springframework.http.HttpStatus
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 import java.util.Collection;
-import java.com.mephiboys.satia.kernel.impl.entitiy.*;
+import com.mephiboys.satia.kernel.impl.entitiy.*
 
 @Controller
 public class SatiaWebController {
@@ -73,7 +76,7 @@ public class SatiaWebController {
         MockedKernelService ks = (MockedKernelService)getKernelService();
         try {
             long testId = Long.parseLong(testIdStr,10);
-            Test test = getTestById(testId);
+            Test test = ks.getTestById(testId);
             model.addObject("test", test);
         }
         catch (NumberFormatException nf) {
